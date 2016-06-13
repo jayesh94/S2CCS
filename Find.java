1023495e1c864cf84b258c0ae5544aeeab0f16c5
jayesh94 <jayesh94nair@gmail.com>
2016-06-13 18:04:04 +0530

    Update Find.java

--- Find.java
+++ Find.java
@@ -1,9 +1,14 @@
 
-/*In this class the detected variables in scilab code are being initialized at Changing.Initializevariable. If the variables have
+/* 1. In this class the detected variables in scilab code are being initialized at Changing.Initializevariable. If the variables have
 already been initialized then no changes are done. The variables are initialized with their data types specified. Data type of a variiable 
 is modified if required Ex. int a; may change to float a; 
-Array length and matrix length is also found out in this class.
- This class is called from other classes as and when a variable is detected. */
+
+2. Array length and matrix length is also found out in this class.
+ This class is called from other classes as and when a variable is detected. 
+ 
+3. If user has selected real time mode and there are arithmetic operations on those
+
+4. Creating new variables at the time of matrix multiplication operation */
 
 package Main;
 import java.util.regex.Matcher;
@@ -14,9 +19,8 @@ public class Find {
 	public static int[] ArrayLength=new int[2];
 	
 	
-	//
+	// 1. To initialize the variables detected
 	public static void FindInitializevariable(String input ,String DataType) {
-		// TODO Auto-generated method stub
 		
 		// if the input is initialized the true Ex. a, OR a; so true
 		if (Changing.Initializevariable.indexOf(input+",")!=-1 || Changing.Initializevariable.indexOf(input+";")!=-1){
@@ -45,7 +49,7 @@ public class Find {
 	}
 	
 	
-	// To find the length of array or matrix Ex. a= [1,2 ; 2,1]
+	// 2. To find the length of array or matrix Ex. a= [1,2 ; 2,1]
 	// Two dimensions of matrix
 	public static void arraylength(String input) {
 		
@@ -89,65 +93,55 @@ public class Find {
 		
 	}
 	
-	// If 
+	//3.  If user has selected real time mode and there are arithmetic operation on those then InputVariableCheck is called.
 	public static void InputVariableCheck(String var1 , String var2) {
 	
-		// 
+		// If var1 is the output varaible specified by user in real time mode. 
+		// It is saved in Changing.InputOutputVariable in form 
+		// $input$output$&
 		if (Pattern.compile("\\$"+var1.replaceAll(" ","").replaceAll("[()]", "")+"\\$\\&").matcher(Changing.InputOutputVariable).find() ) {
 			
-//			if(Pattern.compile("while\\(1\\) \\{").matcher(Changing.MainProgram).find()){
-//				Changing.MainProgram=Changing.MainProgram+"\nwhile((Rcv & I2S0_IR) == 0);\n" +
-//						"while((XmitR & I2S0_IR) == 0);\n" + 
-//						"I2S0_W0_MSW_W = "+var1.replaceAll(" ","").replaceAll("[()]", "")+";\n" + 
-//						"I2S0_W1_MSW_W = "+var1.replaceAll(" ","").replaceAll("[()]", "")+";\n" + 
-//						"\n" + 
-//						"recent_output[j]=output;\n" + 
-//						"j = ((j + 1) % 100);\n";
-//			}
-//			else{
+				// Adding the default code ralated to output in required strings of Changing class
 				Changing.MainProgram=Changing.MainProgram+"\n\n"+"while((XmitR & I2S0_IR) == 0);\n" + 
 						"I2S0_W0_MSW_W = "+var1.replaceAll(" ","").replaceAll("[()]", "")+";\n" + 
 						"I2S0_W1_MSW_W = "+var1.replaceAll(" ","").replaceAll("[()]", "")+";\n" + 
 						"\n" + 
 						"recent_output[j]="+var1.replaceAll(" ","").replaceAll("[()]", "")+";\n" + 
 						"j = ((j + 1) % 100);\n}";
-//			}
-//				Find.FindInitializevariable("recent_output", "Int16");
+
 				Changing.GlobleVariable=Changing.GlobleVariable+"Int16 recent_output[100];\n";
-				Find.FindInitializevariable("j=0", "Int16");
+				Find.FindInitializevariable("j=0", "Int16");// to initialize j
 				Changing.Initializevariable=Changing.Initializevariable+"\n"+Directory.Initializevariable;
 			Changing.InputOutputVariable=Changing.InputOutputVariable.replaceFirst(var1.replaceAll(" ","").replaceAll("[()]", "")+"$", "");
 			
 		}
+		
+		// If var2 is the output varaible specified by user in real time mode. 
+		// It is saved in Changing.InputOutputVariable in form 
+		// $input$output$&
 		else if (Pattern.compile("\\$"+var2.replaceAll(" ","").replaceAll("[()]", "")+"\\$\\&").matcher(Changing.InputOutputVariable).find() ) {
-			
-//			if(Pattern.compile("while\\(1\\) \\{").matcher(Changing.MainProgram).find()){
-//				Changing.MainProgram=Changing.MainProgram+"\nwhile((Rcv & I2S0_IR) == 0);\n" +
-//						"while((XmitR & I2S0_IR) == 0);\n" + 
-//						"I2S0_W0_MSW_W = "+var1.replaceAll(" ","").replaceAll("[()]", "")+";\n" + 
-//						"I2S0_W1_MSW_W = "+var1.replaceAll(" ","").replaceAll("[()]", "")+";\n" + 
-//						"\n" + 
-//						"recent_output[j]=output;\n" + 
-//						"j = ((j + 1) % 100);\n";
-//			}
-//			else{
+				
+				// Adding the default code ralated to output in required strings of Changing class
 				Changing.MainProgram=Changing.MainProgram+"\n\n"+"while((XmitR & I2S0_IR) == 0);\n" + 
 						"I2S0_W0_MSW_W = "+var1.replaceAll(" ","").replaceAll("[()]", "")+";\n" + 
 						"I2S0_W1_MSW_W = "+var1.replaceAll(" ","").replaceAll("[()]", "")+";\n" + 
 						"\n" + 
 						"recent_output[j]="+var1.replaceAll(" ","").replaceAll("[()]", "")+";\n" + 
 						"j = ((j + 1) % 100);\n}";
-//			}
-//				Find.FindInitializevariable("recent_output", "Int16");
+
 				Changing.GlobleVariable=Changing.GlobleVariable+"Int16 recent_output[100];\n";
-				Find.FindInitializevariable("j=0", "Int16");
+				Find.FindInitializevariable("j=0", "Int16");// to initialze j
 				Changing.Initializevariable=Changing.Initializevariable+"\n"+Directory.Initializevariable;
 			Changing.InputOutputVariable=Changing.InputOutputVariable.replaceFirst(var2.replaceAll(" ","").replaceAll("[()]", "")+"$", "");
 			
 		}
 		
+		// If var1 is the input varaible specified by user in real time mode. 
+		// It is saved in Changing.InputOutputVariable in form 
+		// $input$output$&
 		else if (Pattern.compile("\\$"+var1.replaceAll(" ","").replaceAll("[()]", "")+"\\$").matcher(Changing.InputOutputVariable).find() ) {
 			
+			// Adding the default code ralated to input in required strings of Changing class
 			if(Pattern.compile("while\\(1\\) \\{").matcher(Changing.MainProgram).find()){
 				Changing.MainProgram=Changing.MainProgram+"while((Rcv & I2S0_IR) == 0);\n" + 
 						var1.replaceAll(" ","").replaceAll("[()]", "")+" = I2S0_W0_MSW_R;\n" + 
@@ -159,7 +153,7 @@ public class Find {
 						var1.replaceAll(" ","").replaceAll("[()]", "")+" = I2S0_W0_MSW_R;\n" + 
 						var1.replaceAll(" ","").replaceAll("[()]", "")+" = I2S0_W1_MSW_R;\n";
 			}
-//			Find.FindInitializevariable(var1.replaceAll(" ","").replaceAll("[()]", ""), "Int16");
+			
 			Changing.HeaderFile=Changing.HeaderFile+"\n"+Directory.HeaderFile;	
 			Changing.GlobleVariable = Changing.GlobleVariable +"\n"+Directory.GlobleVariable+ "\n" + "void transmit(Int16 a);";
 			Changing.Function=Changing.Function + "\n" +Directory.Function;
@@ -168,8 +162,13 @@ public class Find {
 			Changing.InputOutputVariable=Changing.InputOutputVariable.replaceFirst(var1.replaceAll(" ","").replaceAll("[()]", "")+"\\$", "");
 			
 		}
+		
+		// If var2 is the input varaible specified by user in real time mode. 
+		// It is saved in Changing.InputOutputVariable in form 
+		// $input$output$&
 		else if (Pattern.compile("\\$"+var2.replaceAll(" ","").replaceAll("[()]", "")+"\\$").matcher(Changing.InputOutputVariable).find() ) {
 			
+			// Adding the default code ralated to input in required strings of Changing class
 			if(Pattern.compile("while\\(1\\) \\{").matcher(Changing.MainProgram).find()){
 				Changing.MainProgram=Changing.MainProgram+"while((Rcv & I2S0_IR) == 0);\n" + 
 						var2.replaceAll(" ","").replaceAll("[()]", "")+" = I2S0_W0_MSW_R;\n" + 
@@ -181,7 +180,7 @@ public class Find {
 						var2.replaceAll(" ","").replaceAll("[()]", "")+" = I2S0_W0_MSW_R;\n" + 
 						var2.replaceAll(" ","").replaceAll("[()]", "")+" = I2S0_W1_MSW_R;\n";
 			}
-//			Find.FindInitializevariable(var2.replaceAll(" ","").replaceAll("[()]", ""), "Int16");
+
 			Changing.HeaderFile=Changing.HeaderFile+"\n"+Directory.HeaderFile;
 			Changing.GlobleVariable = Changing.GlobleVariable +"\n"+Directory.GlobleVariable+ "\n" + "void transmit(Int16 a);";
 			Changing.Function=Changing.Function + "\n" +Directory.Function;
@@ -190,54 +189,68 @@ public class Find {
 			Changing.InputOutputVariable=Changing.InputOutputVariable.replaceFirst(var2.replaceAll(" ","").replaceAll("[()]", "")+"\\$", "");
 			
 		}
-		//System.out.println(Changing.InputOutputVariable);
+		
 	}
 	
+	
+	// 
+	// 4. For the purpose of matrix multiplication we require to create new variables i, j and k 
+	// Also the multiplication answer should be saved on a new variable  Ex. Find.CreateVariable("ans", "matrixVariable");
+	// It is being called from class EquationSolver Ex. Find.CreateVariable("i", "variablevalue");
+	// If "i" is already present in Changing.variablevalue then append number to i and increment number if already used 
+	// Ex. i0, i1.... which ever would be available then use the unused variable 
 	public static String CreateVariable(String input ,String variable) {
 		String out = "";
 		if(variable=="variablevalue"){
 			int q;
-			if(!Pattern.compile("("+input+"\\,|"+input+"\\;)").matcher(Changing.variablevalue).find()){
+			
+			// If input is not present in Changing.variablevalue
+			if(!Pattern.compile("\\$"+input).matcher(Changing.variablevalue).find()){
 				out=input;
 				
 			}
 			else {
 				q=0;
-			//	System.out.println(Changing.Initializevariable);
+		
 				do{
-					
+				
 					out=input;
 					out = out+String.valueOf(q);
 					q++;
-				//	System.out.println(i);
-				}while(Pattern.compile(input+"\\,").matcher(Changing.variablevalue).find() && Pattern.compile(input+"\\;").matcher(Changing.Initializevariable).find());
+		
+				}while(Pattern.compile("\\$"+input).matcher(Changing.variablevalue).find());
 			}
 			Find.FindInitializevariable(out, "Int16");
 			Changing.variablevalue=Changing.variablevalue+out+"$";
 		}
 		
+		// The multiplication answer should be saved on a new variable  
+		// For that purpose we have to create a new un used variable 
+		// It is called as Ex. Find.CreateVariable("ans", "matrixVariable");
+		// If "ans" is already present in Changing.matrixVariable then append number to ans and increment number if already used 
+		// Ex. ans0, ans1.... which ever would be available then use the unused variable 
 		if(variable=="matrixVariable"){
 			int q;
-		//	System.out.println(Changing.matrixVariable);
-		//	System.out.println(input);
+			
+			// If input is not present in Changing.matrixVariable
 			if(!Pattern.compile("\\$"+input+"\\[").matcher(Changing.matrixVariable).find()){
 				out=input;
 				
 			}
 			else {
 				q=0;
-		//		System.out.println(Changing.matrixVariable);
+		
 				do{
 					
 					out=input;
 					out = out+String.valueOf(q);
 					q++;
-				//	System.out.println(out);
+		
 				}while(Pattern.compile("\\$"+out+"\\[").matcher(Changing.matrixVariable).find());
 			}
 			
 		}
-		//System.out.println(out);
+	
 		return out;
 	}
 
