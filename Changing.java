b84d6b579b1b5684639589de90d861cf7c8c013c
jayesh94 <jayesh94nair@gmail.com>
2016-06-13 12:31:26 +0530

    Update Changing.java

--- Changing.java
+++ Changing.java
@@ -1,5 +1,8 @@
 
-/*Here MainProgram is used to store the converted code at the steps of code detection in MainGrammar.jj*/
+/*Here MainProgram is used to store the converted code at the steps of code detection in MainGrammar.jj
+  All the headers, global variables etc. required for initialization of the corresponding dsp kit selected is stored to 
+  the strings in this class pefore appending to main program. This provides the flexibility of adding n number of initialization code 
+  of any compatible dsp kit in the class Directory. */
 
 package Main;
 
